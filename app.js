2f98adbd84d4e5ca408699126c7178d19e1b440a
Shashank Shekhar <shshankm597@gmaail.com>
2020-12-12 04:58:37 +0530

    JS done

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var output = document.querySelector("#outputDiv");
 
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// translation API
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
 
 function getTranslationURL(input) {
-    return serverUrl = "?" + "text=" + input;
+    return serverUrl + "?" + "text=" + input;
+}
+
+// custom messages for the error 
+function errorHandler(error) {
+    console.log("Error Occured", error);
+    alert("Something went wrong with the server!!! Please try after sometime");
+
 }
 
 function clickHandler() {
@@ -14,7 +23,11 @@ function clickHandler() {
 
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
-        .then(json => console.log(json.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler) //catch block to handle the errors
 }
 
 
